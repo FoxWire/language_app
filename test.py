3ed51ca36fe9bfb61b7f7d97e1160c3de38d27ee
stuart.miller@gmail.com <stuart.miller60@gmail.com>
2018-07-23 11:31:46 +0100

    test.py has code to convery parse tree to zzs tree for comparison

--- test.py
+++ test.py
@@ -12,108 +12,116 @@ import json
 import re
 
 
-one = (
-        Node("ROOT")
-            .addkid(Node('S')
-                .addkid(Node('VP')
-                            .addkid(Node('VB'))
-                            .addkid(Node('RB'))
-                            .addkid(Node('VP')
-                                .addkid(Node('VB'))
-                                .addkid(Node('NP')
-                                    .addkid(Node('PRP$'))
-                                    .addkid(Node('NN')))
-                                .addkid(Node('PP')
-                                    .addkid(Node("IN"))
-                                    .addkid(Node('NP')
-                                        .addkid(Node("NN")))
-                                    )
-                                )))
+A = (
+    Node("f")
+        .addkid(Node("a")
+            .addkid(Node("d"))
+            .addkid(Node("c")
+                .addkid(Node("b"))))
+        .addkid(Node("e"))
+    )
+B = (
+    Node("f")
+        .addkid(Node("a")
+            .addkid(Node("d"))
+            .addkid(Node("c")
+                .addkid(Node("b"))))
+        .addkid(Node("e"))
     )
 
-two = (
-        Node("ROOT")
-            .addkid(Node('S')
-                .addkid(Node('VP')
-                            .addkid(Node('VB'))
-                            .addkid(Node('RB'))
-                            .addkid(Node('VP')
-                                .addkid(Node('VB'))
-                                .addkid(Node('NP')
-                                    .addkid(Node('PRP$'))
-                                    .addkid(Node('NN')))
-                                .addkid(Node('PP')
-                                    .addkid(Node("IN"))
-                                    .addkid(Node('NP')
-                                        .addkid(Node("NN")))
-                                    )
-                                )))
+test_tree = (
+    Node("ROOT")
+        .addkid(Node("S")
+            .addkid(Node("NP")
+                .addkid(Node("PRP")
+                    .addkid(Node("I"))))
+            .addkid(Node("VP")
+                .addkid(Node("VBP")
+                    .addkid(Node("am"))))
+                .addkid(Node("NP")
+                    .addkid(Node("NNP")
+                        .addkid(Node("Stuart")))))
     )
 
-
-
-# A = (
-#     Node("f")
-#         .addkid(Node("a")
-#             .addkid(Node("h"))
-#             .addkid(Node("c")
-#                 .addkid(Node("l"))))
-#         .addkid(Node("e"))
-#     )
-# B = (
-#     Node("f")
-#         .addkid(Node("a")
-#             .addkid(Node("d"))
-#             .addkid(Node("c")
-#                 .addkid(Node("b"))))
-#         .addkid(Node("e"))
-#     )
-# print(simple_distance(one, two))
 
 
 parser = stanford.StanfordParser()
 
 # These are the sentences to be parsed
-chunk_a = 'I am Stuart' 
-# chunk_b = 'This is just a test sentence'
+chunk = 'I am Stuart' 
 
 # parse the sentences
-tree_a = next(parser.raw_parse(chunk_a))
-# tree_b = next(parser.raw_parse(chunk_b))
+tree = next(parser.raw_parse(chunk))
 
-tree_a.pretty_print()
+tree.pretty_print()
 
-# You may just have to iterate over the string representation
-string_rep = str(tree_a)
+tree_as_string = str(tree)
+print(tree_as_string)
+
+tree_as_list = [item.strip() for item in re.split(r'([\(\)])', tree_as_string) if item.strip()]
+# At this point the string is processed and ready to be worked on.
+
+def convert_parse_tree_to_python_tree(tree_as_list):
+    tree_as_list = tree_as_list[2:-1]
+    stack = [ ['ROOT', []],  ]
+    root = stack[0]
+    # Iterate over the list
+    for i, item in enumerate(tree_as_list):
+        if item == '(':
+
+            # If the node doesn't have children
+            match = re.search(r'[A-Z]+[ ][A-Za-z]+', tree_as_list[i + 1])
+            if match:
+                label = match.group().split(' ')
+                node = [label[0], label[1]]
+            else:
+                node = [tree_as_list[i + 1], []]
+
+            # Add the node to the children of the current item
+            stack[-1][1].append(node)
+            # Then add the node to the stack itself
+            stack.append(node)
+        elif item == ')':
+            # this node has no children so just pop it from the stack
+            stack.pop()
+    return root
 
 
-string_rep = "".join(string_rep.split('\n'))
-string_rep = string_rep.replace('(', '[').replace(')', ']')    
 
 
-print(string_rep)
+def convert_parse_tree_to_zss_tree(tree_as_list):
+    tree_as_list = tree_as_list[2:-1]
+    stack = [Node('ROOT')]
+    root_node = stack[0]
+    # Iterate over the list
+    for i, item in enumerate(tree_as_list):
+        if item == '(':
+            # match the string for each item 
+            match = re.search(r'[A-Z]+[ ][A-Za-z]+', tree_as_list[i + 1])
+            if match:
+                # if match, node has no children
+                label = match.group().split(' ')
+                node = Node(label[0]).addkid(Node(label[1]))
+            else:
+                # otherwise node has children
+                node = Node(tree_as_list[i + 1])
+            # Add the node to the children of the current item
+            stack[-1].addkid(node)
+            # Then add the node to the stack itself
+            stack.append(node)
+        elif item == ')':
+            # this node has no children so just pop it from the stack
+            stack.pop()
+    return root_node
 
-string_b = "[S [NP [PRP I]] [VP [VBP am] [NP [NNP Stuart]]]]"
 
-string_a = "[X [X [X None]] [X [X None] [X [X None]]]]"
+zss_tree = convert_parse_tree_to_zss_tree(tree_as_list)
 
-x = re.split(r'([^A-Za-z])', string_b)
-list_b = [y for y in x if y and y != ' ']
+print(simple_distance(zss_tree, test_tree))
+print()
+print(zss_tree)
+print()
+print(test_tree)
 
-stack = [ ['ROOT', []],  ]
-root = stack[0]
-# Iterate though the string 
-for i, item in enumerate(list_b):
-    if item == '[':
-        node = [list_b[i + 1], [], ]
-        # Add the node to the children of the current item
-        stack[-1][1].append(node)
-        # Then add the node to the stack itself
-        stack.append(node)
-    elif item == ']':
-        # this node has no children so just pop it from the stack
-        stack.pop()
-
-import pdb; pdb.set_trace()
 
 
